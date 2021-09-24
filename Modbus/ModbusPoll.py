12b0b0ecce580b0d7ac8bf0759007c4446433582
shaker96 <shakerchabarekh@gmail.com>
2021-09-23 23:11:35 -0400

    modbuspoll

--- Modbus/ModbusPoll.py
+++ Modbus/ModbusPoll.py
@@ -1,10 +1,11 @@
 import minimalmodbus as modbus
 from Publisher import send_data
-from DBActions import getActuators
+from Consumer import receiveData
+from DBActions import getActuators, storeReading
 
 def run():
     actuator_arr = getActuators() # obtener actuadores desde la BDD
-    reader = modbus.Instrument('COM1', 0, mode='rtu', close_port_after_each_call=False, debug=True)
+    reader = modbus.Instrument('COM1', 0, mode='rtu', close_port_after_each_call=False, debug=False)
     reader.serial.baudrate = 9600
     for addr in actuator_arr:
         reader.address = addr
@@ -12,13 +13,14 @@ def run():
         while flag < 10: # 10 intentos
             try:
                 res = reader.read_registers(4, 11)
-                send_data([addr] + res)
+                print('Lectura exitosa. dir. Modbus ' + str(addr) + '.\nRespuesta: ' + str(res))
+                storeReading([addr] + res)
                 flag = 10
             except:
                 flag += 1
-                print('error')
+                print('Intento #' + str(flag) + '. Error de conexión con dir. Modbus ' + str(addr))
                 if flag >= 10:
-                    send_data([addr]) # consulta fallida. envia solo direccion modbus
+                    storeReading([addr]) # consulta fallida. envia solo direccion modbus
 
 if __name__ == "__main__": # correr cada 10 seg para consultar data
     run()
